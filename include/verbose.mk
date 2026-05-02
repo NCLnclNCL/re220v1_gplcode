57c3e702d9f72703ba0adcddca3c20d718dbd514
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-02 18:23:03 +0700

    Update verbose.mk

--- include/verbose.mk
+++ include/verbose.mk
@@ -51,8 +51,8 @@ ifeq ($(findstring s,$(OPENWRT_VERBOSE)),)
       SUBMAKE:=cmd() { $(MAKE) $$* && printf "$(_Y) make $$* finished$(_N)\n" >&8 || { printf "$(_Y) make $$* failed$(_N)\n" >&8; false; } }; cmd
     endif
   else
-    SILENT:=>/dev/null $(if $(findstring w,$(OPENWRT_VERBOSE)),,2>&1)
-    export QUIET:=1
+    SILENT:
+    export QUIET:=0
     SUBMAKE=cmd() { $(SILENT) $(MAKE) -s $$* < /dev/null || { echo "make $$*: build failed. Please re-run make with V=s to see what's going on"; false; } } 8>&1 9>&2; cmd
   endif
 
